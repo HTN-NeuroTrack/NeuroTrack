61c9d35706e53620cac9c74f375432592dc9b64d
Samir Sharma <samirraisharma@gmail.com>
2024-09-15 15:54:13 -0600

    Update TeamMembers.tsx

--- pg-fe/src/components/TeamMembers.tsx
+++ pg-fe/src/components/TeamMembers.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 
 const teamMembers = [
-  { name: 'Samir Sharma', linkedin: 'https://www.linkedin.com/in/samirrsharma' },
+  { name: 'Samir Sharma', linkedin: 'https://www.linkedin.com/in/samir-rsharma/' },
   { name: 'Kevin Dang', linkedin: 'https://www.linkedin.com/in/kevin-dang-a922712ab/' },
   { name: 'Noah Levy', linkedin: 'https://www.linkedin.com/in/noah-levy-576295302/' },
   { name: 'Aadi Umrani', linkedin: 'https://www.linkedin.com/in/aadi-umrani' }
