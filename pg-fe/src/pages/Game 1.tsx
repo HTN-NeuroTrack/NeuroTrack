ebf5e4d56cf96b564dc83c945cab555d34fbb330
Aadi Umrani <aadi.p.um@gmail.com>
2024-09-15 05:03:41 -0400

    Update file paths in Game 1.tsx and vercel.json

--- pg-fe/src/pages/Game 1.tsx
+++ pg-fe/src/pages/Game 1.tsx
@@ -4,7 +4,7 @@ const UnityPage1: React.FC = () => {
   return (
     <div className="flex justify-center items-center h-screen bg-gray-100">
       <iframe
-        src="../../Public1/index.html" 
+        src="/index.html" 
         width="100%"
         height="100%"
         style={{ border: 'none' }}
