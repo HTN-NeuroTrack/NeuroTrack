89cbc6a132fdac18e1a9b0c0a8aee006c609f500
Aadi Umrani <aadi.p.um@gmail.com>
2024-09-15 07:43:31 -0400

    Update file paths in Game 1.tsx and Game 2.tsx

--- pg-fe/src/pages/Game 2.tsx
+++ pg-fe/src/pages/Game 2.tsx
@@ -4,7 +4,7 @@ const UnityPage2: React.FC = () => {
   return (
     <div className="flex justify-center items-center h-screen bg-gray-100">
       <iframe
-        src={"../../Public/Public2/index.html"}
+        src={"/Public2/index.html"}
         width="100%"
         height="100%"
         style={{ border: 'none' }}
