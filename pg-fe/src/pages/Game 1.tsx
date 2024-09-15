7fdbc78e25004c17c5f7e46d2a83eba439393afc
Samir Sharma <samirraisharma@gmail.com>
2024-09-15 05:14:38 -0400

    blank

--- pg-fe/src/pages/Game 1.tsx
+++ pg-fe/src/pages/Game 1.tsx
@@ -4,7 +4,6 @@ const UnityPage1: React.FC = () => {
   return (
     <div className="flex justify-center items-center h-screen bg-gray-100">
       <iframe
-
         src={"../../Public1/index.html"}
         width="100%"
         height="100%"
